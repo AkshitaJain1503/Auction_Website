03204780a7dbbb5e47c45c2b24cdcdc7c6faaf52
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2023-02-17 21:38:18 +0530

    added post product feature

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,9 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/home";
 import Signup from "./components/loginAndSignup/signup";
 import Login from "./components/loginAndSignup/login";
-
+import MyProfile from "./components/myProfile";
+import PostProduct from "./components/postProduct/postProduct";
+import ProductPage from "./components/postProduct/productPage";
 function App() {
 	const user = localStorage.getItem("token");
 
@@ -12,6 +14,9 @@ function App() {
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
+			{ user && <Route path="/myProfile" exact element={<MyProfile />}/>}
+			{user && <Route path="/postProduct" exact element={<PostProduct/>}/>}
+			{user && <Route path="/productPage" exact element={<ProductPage/>}/>}
 		</Routes>
 	);
 }
---
+++ client/src/components/postProduct/productPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProductPage() {
+  return (
+    <div>
+      <h1>Individual Product Page</h1>
+    </div>
+  )
+}
