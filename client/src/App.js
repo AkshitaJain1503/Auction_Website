ba1fb3d461edd41e6430725da4f7776a635f07fb
Savneet <2021ucs0115@iitjammu.ac.in>
2023-02-17 07:42:21 +0530

    added buttons

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./components/home";
 import Signup from "./components/loginAndSignup/signup";
 import Login from "./components/loginAndSignup/login";
-
+import MyProfile from "./components/myProfile";
+import PostProduct from "./components/postProduct";
 function App() {
 	const user = localStorage.getItem("token");
 
@@ -12,6 +13,8 @@ function App() {
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
+			{ user && <Route path="/myProfile" exact element={<MyProfile />}/>}
+			{user && <Route path="/postProduct" exact element={<PostProduct/>}/>}
 		</Routes>
 	);
 }
