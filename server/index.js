43e68fba2b46f4726fcd8e8a9d9b1d9ed67eadca
AkshitaJain1503 <97738278+AkshitaJain1503@users.noreply.github.com>
2023-03-04 18:40:57 +0530

    route fixing

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use("/api/myProfile", checkAuthLogin, myProfile);
 app.use("/api/pastPosts", checkAuthLogin, pastPosts);
 app.use("/api/pastPurchases", checkAuthLogin, pastPurchases);
 app.use("/api/postProduct", checkAuthLogin, postProduct);
-app.use("/api/productDetails", productDetails);
+
 
 
 const port = process.env.PORT || 3001;
