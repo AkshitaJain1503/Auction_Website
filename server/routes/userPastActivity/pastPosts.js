f6cc72f9253fecaa43f1a0d341f8808be41423e2
AkshitaJain1503 <97738278+AkshitaJain1503@users.noreply.github.com>
2023-03-06 16:13:59 +0530

    past purchases component (#17)

--- server/routes/userPastActivity/pastPosts.js
+++ server/routes/userPastActivity/pastPosts.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const {User} = require("../../models/user");
 const {Product} = require("../../models/product");
+const { Auction } = require("../../models/auction");
 
 
 //Get past products posted as data value of array of objects.
@@ -8,11 +9,11 @@ router.get("/", async (req, res) => {
 
     let requested_id;
 
-//if the user wants his own past posts, then no id in param in the url.
+//if the user wants his own past posts, then no id in params in the url.
     if(req.query.id == "null"){
-        requested_id = req.id;
+        requested_id = req.id;  //id from middleware
     }
-//if the past posts of the buyer or seller is demanded, then yes id in param in the url.
+//if the past posts of the buyer or seller is demanded, then yes id in params in the url.
     else{
         requested_id = req.query.id;
     }
@@ -26,15 +27,25 @@ router.get("/", async (req, res) => {
 
     // to avoid hitting the DB query in a loop, writing a SQL equivalent IN query. 
     let products = await Product.find({_id: postedProducts});
+    let auction = await Auction.find({product: postedProducts});
 
     //loop from last to display the latest posts on top
     for(var i=products.length-1 ; i >=0; i--){
 
         var productDetails = {};
+
         productDetails.SNo = products.length-i;
         productDetails.productId = products[i]._id;
         productDetails.productName = products[i].productName;
         productDetails.basePrice = products[i].productBasePrice;
+
+        let formattedStartTime= new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',
+        day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(auction[i].startDateTime);
+        let formattedEndTime= new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',
+        day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(auction[i].endDateTime);
+
+        productDetails.startTime= formattedStartTime;
+        productDetails.endTime= formattedEndTime;
 
         responseData.push(productDetails);
     }
