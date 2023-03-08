4e120b1b766d52cd0b6a38af85490e8ca3acc380
Rishika Saria <s.rishika@iitg.ac.in>
2023-03-08 23:31:28 +0530

    added search result cards

--- client/src/components/search_page/search.jsx
+++ client/src/components/search_page/search.jsx
@@ -5,6 +5,12 @@ import { useNavigate } from "react-router-dom";
 import { TableControl } from 'react-bootstrap-table-control';
 import NavBar from "../navbar/index";
 
+//import React, { Component ,useEffect} from 'react';
+import { ReactiveList } from '@appbaseio/reactivesearch';
+import './App.css';
+
+
+// this function returns name that has been searched for by the user
 const GetProductname = () => {
     const useQuery = () => new URLSearchParams(useLocation().search);
     const query = useQuery();
@@ -12,7 +18,7 @@ const GetProductname = () => {
     
     return name;
 }
-
+//fetches all products that match the name from GetProductname()
 const GetSearchResults = () => {
     const name = GetProductname();
     const [data, setData] = React.useState({})
@@ -35,36 +41,85 @@ const GetSearchResults = () => {
 
 //Renders the past purchases in a table whose each row on cliking renders the specific product page.
 //(TableControl) 25 rows are shown on one page. Rest are shown via pagination
-const SearchDetails = () => {
-    const navigate = useNavigate();
-    let data = GetSearchResults()
+// const SearchDetails = () => {
+//     const navigate = useNavigate();
+//     let data = GetSearchResults()
     
-    if(Object.keys(data).length > 0){
-        return (
-            <div>
-                <NavBar/>
+//      if(Object.keys(data).length > 0){
+//         return (
+//             <div>
+//                 <NavBar/>
+//                 <h5>Total Matching Products:  {data.length}</h5>
+//                 <hr/>
+//                  <TableControl
+//                     header={[
+//                     { key: "SNo", name: "#" },
+//                     { key: "productName", name: "Product Name" },
+//                     { key: "basePrice", name: "Base Price" }
+//                     ]}
+//                     itens={data}
+//                     onClickdata={function navProduct(data){
+//                         navigate(`/productPage?id=${data.productId}`);
+//                     }}
+
+//                     clickable
+//                     tableProps={{
+//                         hover: true,
+//                     }}
+//                     totalPosition="none"
+//                 /> 
+                
+//             </div>
+//         );
+//     }
+    const SearchDetails = () => {
+        const navigate = useNavigate();
+        let data = GetSearchResults()
+        
+         if(Object.keys(data).length > 0){
+            return (
+                <div>
+                 <NavBar/>
                 <h5>Total Matching Products:  {data.length}</h5>
                 <hr/>
-                <TableControl
-                    header={[
-                    { key: "SNo", name: "#" },
-                    { key: "productName", name: "Product Name" },
-                    { key: "basePrice", name: "Base Price" }
-                    ]}
-                    itens={data}
-                    onClickItem={function navProduct(data){
-                        navigate(`/productPage?id=${data.productId}`);
-                    }}
-
-                    clickable
-                    tableProps={{
-                        hover: true,
-                    }}
-                    totalPosition="none"
-                />
+                
+                <ul className="card-grid" onClickdata={function navProduct(data){
+                         navigate(`/productPage?id=${data.productId}`);
+                     }}>
+                    {data.map((data) => (
+                        <li>
+                            <article className="card" key={data.productId}>
+                                <div className="card-image">
+                                    <img src={data.img} alt={data.productName} />
+                                </div>
+                                <div className="card-content">
+                                    <h2 className="card-name">{data.productName}</h2>
+                                    <ol className="card-list">
+                                        <ul>
+                                        Base Price : 
+                                            { <span>{ data.basePrice}</span> }
+                                        </ul>
+                                        <ul>
+                                        Shipment From: {data.shipment}
+                                        </ul>
+                                        <ul>
+                                        Start Time: {data.StartTime}
+                                        </ul>
+                                        <ul>
+                                        End Time: {data.EndTime}
+                                        </ul>
+                                    </ol>
+                                </div>
+                            </article>
+                        </li>
+                    ))}
+                </ul>
             </div>
-        );
+            
+            );
     }
+            
+        
     else{
         return (
             <div>
@@ -74,5 +129,34 @@ const SearchDetails = () => {
         );
     }
 }
-
+// const SearchDetails = () => {
+//     const navigate = useNavigate();
+//     let data = GetSearchResults();
+//     const handleNavigate = (data) => {
+//       navigate(`/productPage?id=${data.productId}`);
+//     };
+  
+//     return (
+//       <div>
+//         <NavBar />
+//         <h5>Total Matching Products: {data.length}</h5>
+//         <hr />
+//         <ReactiveList
+//           componentId="productId"
+//           dataField="productName"
+//           react={{
+//             and: ['productName'],
+//           }}
+//           renderdata={(data) => (
+//             <div key={data.productId} onClick={() => handleNavigate(data)}>
+//                 <h3>{data.productName}</h3>
+//             <p>{data.SNo}</p>
+//             <img src={data.img} alt={data.productName} />
+//             <p>Base Price: {data.basePrice}</p>
+//           </div>
+//         )}
+//       />
+//     </div>
+//   );
+// };
 export default SearchDetails;
--- server/routes/search_page/search.js
+++ server/routes/search_page/search.js
@@ -1,106 +1,32 @@
 const router = require("express").Router();
 const {Product} = require("../../models/product");
+const { Auction } = require("../../models/auction");
 router.get("/", async (req, res) => {
 
     const requestedProductName = req.query.name;
-    var responseData = [];
+    var responseData = []; //this is an array of objects
     
     let products = await Product.find({ productName :{ $regex : '.*'+ requestedProductName + '.*', $options: 'i' }});
+    //let auction = await Auction.find({ productName :{ $regex : '.*'+ requestedProductName + '.*', $options: 'i' }});
     for(var i=products.length-1 ; i >=0; i--){
 
-        var productDetails = {};
+        var productDetails = {}; // this will hold a single object
 
         productDetails.SNo = products.length-i;
         productDetails.productId = products[i]._id;
         productDetails.productName = products[i].productName;
         productDetails.basePrice = products[i].productBasePrice;
+        productDetails.img = products[i].productImage;
+        productDetails.shipment = products[i].shipmentFrom;
+        // let formattedEndTime= new Intl.DateTimeFormat('en-GB', {year: 'numeric', month: '2-digit',
+        // day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(auction[i].endDateTime);
+        // let formattedStartTime= new Intl.DateTimeFormat('en-GB', {year: 'numeric', month: '2-digit',
+        // day: '2-digit', hour: '2-digit', minute: '2-digit'}).format(auction[i].endStartTime)
+        // productDetails.EndTime= formattedEndTime;
+        // productDetails.StartTime= formattedStartTime;
+
         responseData.push(productDetails);
     }
- res.status(200).send({data:responseData});
+ res.status(200).send({data:responseData}); //returns the array of objects
 });
 module.exports = router;
-/* const router = require("express").Router();
-const {Product} = require("../../models/product");
-const {Auction} = require("../../models/auction");
-
-
-//Get the buyer or seller (user) profile data.
-router.get("/", async (req, res) => {
-
-    const requestedProductName = req.query.name;
-    var productdata = {}
-
-    // to avoid hitting the DB query in a loop, writing a SQL equivalent IN query.
-    //writing a SQL equivalent LIKE Operator query.
-    //$options: 'i' ensures case insensitivity.
-    let ProductList = await Product.find({ productName :{ $regex : '.'+ requestedProductName + '.', $options: 'i' }});
-    // let ProductIdList = [];
-    // for(var i=0 ; i < ProductList.length; i++){
-    //     let ProductId = ProductList[i]._id;
-    //     ProductIdList.push(ProductId);
-    // }
-
-    // let AuctionList = await Auction.find({product: ProductIdList});
-
-    // let response = [];
-    // let auctionData = {};
-
-    // //returns {pId:Date, pId:Date, ....}
-    // for(var i=0 ; i < AuctionList.length; i++){
-    //     let ProductId = AuctionList[i].product;
-    //     auctionData[ProductId] = AuctionList[i].startDateTime;
-    // }
-
-    // //returns [{date: date,title: name, color: colour},{},{},....]
-    // for(var i=0 ; i < ProductList.length; i++){
-    //     let resData = {};
-    //     let prodData = ProductList[i];
-    //     prodID = prodData._id;
-    //     let name = prodData.productName;
-    //     let auctDate = auctionData[prodID];
-    //     resData.date = auctDate;
-    //     resData.title = name;
-    //     // var dateObj = new Date(auctDate);
-    //     // var month = dateObj.getUTCMonth(); //months from 1-12
-    //     // var date = dateObj.getUTCDate();
-    //     // var year = dateObj.getUTCFullYear();
-    //     // var resDate = new Date(year, month, date)
-    //     // console.log(dateObj,"dateObj");
-    //     // console.log(auctDate,"auctDate");
-    //     // resData.date = resDate;
-
-    //     // let indianDateTody = JustADate(new Date()).toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'});
-    //     // let auctDateCompare = JustADate(new Date(auctDate)).toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'});
-    //     // let auctDateCompare = new Date(auctDate).toLocaleString("en-Us", {timeZone: 'Asia/Kolkata'});
-    //     // console.log(indianDateTody,"indianDateToday");
-    //     // console.log(auctDateCompare,"resDate");
-        
-    //     console.log(auctDate,"auctDate");
-    //     // console.log(new Date(auctDate),"auctDateNew");
-    //     let todayDate = new Date();
-    //     console.log(todayDate,"todayDate");
-    //     // let indianDateTody= new Date().toISOString().split("T")[0];
-    //     // let auctDateCompare = new Date(auctDate).toISOString().split("T")[0];
-    //     // console.log(indianDateTody,"indianDateToday");
-    //     // console.log(auctDateCompare,"resDate");
-    //     // console.log("compare", indianDate> resDateCompare);
-    //     let colour;
-    //     if(todayDate < auctDate){
-    //         colour = "#774dbf";
-    //     }
-    //     else if(todayDate > auctDate){
-    //         colour = "#bf6d4d";
-    //     }
-    //     else{
-    //         colour = "#9bbf4d";
-    //     }
-    //     resData.color = colour;
-
-    //     response.push(resData)
-    // }
-
-    res.status(200).send({data:ProductList});
-});
-
-
-module.exports = router;*/
