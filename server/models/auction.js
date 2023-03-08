1acd03935ac543469d05ef9ec8c93b81c92975ac
Vishwa <89728789+vishwa-sadula@users.noreply.github.com>
2023-03-08 16:56:59 +0530

    modified auction db (#23)

--- server/models/auction.js
+++ server/models/auction.js
@@ -6,6 +6,10 @@ const auctionSchema = new mongoose.Schema({
         ref: 'Product',
         required: true
     },
+    productName:{
+        type: String,
+        required : true
+    },
     bids: [ {
         bidder: {
             type: mongoose.Types.ObjectId,
