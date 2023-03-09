d170f953dec9eb056c43856a87c1f50310b5a4b0
vishwa-sadula <sadula.vishwa.cse20@itbhu.ac.in>
2023-03-09 20:05:40 +0530

    used navigate in calendar details

--- client/src/components/calendarView/calendarDetails.jsx
+++ client/src/components/calendarView/calendarDetails.jsx
@@ -1,11 +1,12 @@
 import React,{ useEffect,useState} from 'react';
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import NavBar from "../navbar/index";
 import { Link } from 'react-router-dom';
 import styles from "../auctionSpace/styles.module.css";
 import './calendarDetails.css';
 
  const CalendarDetails = () => {
+    const navigate = useNavigate();
     const useQuery = () => new URLSearchParams(useLocation().search);
     const query = useQuery();
     const productName = query.get('name');
@@ -23,6 +24,10 @@ import './calendarDetails.css';
           .catch(error => console.error(error));
       }, []);
 
+      const handleOnClickEvent = (product) => {
+        navigate(`/productPage?id=${product.productId}`  );
+      };
+
     return (
         <div>
             <NavBar />
@@ -32,33 +37,35 @@ import './calendarDetails.css';
             <hr />
             {/* <div className={styles.backGroundSpace}> */}
             <ul className="card-grid">
-                {data.map((data) => (
-                    <li>
-                        <Link to={`/productPage?id=${data.productId}`} className="card-link">
-                            <article className="card" key={data.productId}  >
+                {data.map((product) => (
+                    <li key={product.productId}>
+                        {/* <Link to={"/"} className="card-link"> */}
+                            <div onClick={() => handleOnClickEvent(product)} >
+                            <article className="card" key={product.productId}  >
                             <div className="card-image">
-                                <img src={data.img} alt={data.productName} />
+                                <img src={product.img} alt={product.productName} />
                             </div>
                             <div className="card-content">
-                                <h2 className="card-name">{data.productName}</h2>
+                                <h2 className="card-name">{product.productName}</h2>
                                 <ol className="card-list">
                                     <ul>
                                         Base Price :
-                                        { <span>{ data.basePrice}</span> }
+                                        { <span>{ product.basePrice}</span> }
                                     </ul>
                                     <ul>
-                                        Shipment From: {data.shipment}
+                                        Shipment From: {product.shipment}
                                     </ul>
                                     <ul>
-                                        Start Time: {data.StartTime}
+                                        Start Time: {product.StartTime}
                                     </ul>
                                     <ul>
-                                        End Time: {data.EndTime}
+                                        End Time: {product.EndTime}
                                     </ul>
                                 </ol>
                                 </div>
                         </article>
-                    </Link>
+                        </div>
+                    {/* </Link> */}
                 </li>
             ))}
         </ul> 
