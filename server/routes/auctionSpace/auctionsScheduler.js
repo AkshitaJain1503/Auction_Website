6828ac80de6c5fc7c94595d1ad5c7a9deb126d54
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2023-03-17 22:09:08 +0530

    email-notifications done fully

--- server/routes/auctionSpace/auctionsScheduler.js
+++ server/routes/auctionSpace/auctionsScheduler.js
@@ -1,121 +1,249 @@
-const schedule = require('node-schedule');
-const {Auction} = require("../../models/auction");
+const schedule = require("node-schedule");
+const nodemailer = require("nodemailer");
+const { Auction } = require("../../models/auction");
+const { Product } = require("../../models/product");
 const { User } = require("../../models/user");
 
-async function startAuction(auction){
+// declaring global variables for usage across different functions
+var subscribers = "";
+let sellerEmail = "";
+let soldToEmail = "";
+let soldToName = "";
+let sellerName = "";
+let productCurrentPrice = 0;
+
+async function findSubscribersOfThisProduct(auction) {
+  // finds subscribers of the given auction product and saves it in the global string subscribers
+  const auctionOfThatProduct = await Auction.findOne({ _id: auction._id });
+  const subscribersOfThatProduct = auctionOfThatProduct.subscribers;
+  const userDBOfSubscribers = await User.find({
+    _id: subscribersOfThatProduct,
+  });
+  for (let i = 0; i < userDBOfSubscribers.length; i++) {
+    subscribers += userDBOfSubscribers[i].email + ", ";
+  }
+}
+
+async function findSoldToBuyer(auction) {
+  // finds buyer of that particular auction product after auction ends
+  const auctionOfThatProduct = await Auction.findOne({ _id: auction._id });
+  const buyerID = auctionOfThatProduct.currentBidder;
+  const buyer = await User.findOne({ _id: buyerID });
+  if (buyer) {
+    soldToName = buyer.name;
+    soldToEmail = buyer.email;
+  }
+}
+
+async function sellerOfThisProduct(auction) {
+  // finds the seller of the particular auction product
+  const product = await Product.findOne({ _id: auction.product });
+  const sellerID = product.seller;
+  const seller = await User.findOne({ _id: sellerID });
+  sellerEmail = seller.email;
+  sellerName = seller.name;
+}
+
+const emailNotification = (receiver, subject, text) => {
+  // sends email notification to the receiver using nodemailer
+
+  // transporter stores the email provider's details
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "auctionwebsitedesisproject@gmail.com",
+      pass: "mcwxzkykchkgwyaj",
+    },
+  });
+
+  // mailoptions stores the receiver and sender's mail id and the subject along with inside content or text
+  var mailOptions = {
+    from: "auctionwebsitedesisproject@gmail.com",
+    to: "shreyasristi2003@gmail.com",
+    subject: subject,
+    text: text,
+  };
+
+  // email gets sent using sendMail() function
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+};
+
+async function startAuction(auction) {
   await Auction.findOneAndUpdate(
     { _id: auction._id },
     {
       auctionStarted: true,
       auctionLive: true,
-    });
+    }
+  );
 }
 
-async function endAuction(auction){
+async function endAuction(auction) {
   // storing the current (latest) data of this auction and updating it
-  const currAuction= await Auction.findOneAndUpdate(
-    { _id: auction._id }, 
-    { 
-        auctionEnded: true,
-        auctionLive: false,
-    }
-)
-// check to see if there is atleast 1 bid, at the end time (i.e., in current auction )
-if( currAuction.bids.length )
+  const currAuction = await Auction.findOneAndUpdate(
+    { _id: auction._id },
     {
-        const maxBidder= currAuction.currentBidder;
-        // product sold to the max bidder
-        await Auction.findOneAndUpdate(
-            { _id: auction._id }, 
-            { 
-                soldTo: maxBidder
-            }
-        )
-        // adding this product in the purchased products array of the highest bidder
-        await User.findOneAndUpdate(
-            {_id: maxBidder},
-            {
-                $push: { purchasedProducts: auction.product }
-            });
+      auctionEnded: true,
+      auctionLive: false,
     }
-
+  );
+  // check to see if there is atleast 1 bid, at the end time (i.e., in current auction )
+  if (currAuction.bids.length) {
+    const maxBidder = currAuction.currentBidder;
+    productCurrentPrice = currAuction.productCurrentPrice;
+    // product sold to the max bidder
+    await Auction.findOneAndUpdate(
+      { _id: auction._id },
+      {
+        soldTo: maxBidder,
+      }
+    );
+    // adding this product in the purchased products array of the highest bidder
+    await User.findOneAndUpdate(
+      { _id: maxBidder },
+      {
+        $push: { purchasedProducts: auction.product },
+      }
+    );
+  }
 }
 
-async function scheduleStart(auction){
-  if(auction.startDateTime<new Date()){
+async function scheduleReminder(auction) {
+  // finds the reminder date and time based on 24 hours difference from start date and time
+  const reminderDateTime = new Date(
+    new Date(auction.startDateTime).getTime() - 60 * 60 * 24 * 1000
+  );
+  // schedules the reminder date and time using scheduleJob attribute of node-schedule npm package
+  const reminderAuctionJob = schedule.scheduleJob(
+    reminderDateTime,
+    async function () {
+      // finds the subscribers of this product
+      findSubscribersOfThisProduct(auction);
+      // sends emails to the subscribers
+      emailNotification(
+        subscribers,
+        "Reminder! Auction is going to start in 24 hours",
+        `The auction of ${auction.productName} is going to start in 24 hours`
+      );
+    }
+  );
+}
+
+async function scheduleStart(auction) {
+  // finds subscribers of the product
+  findSubscribersOfThisProduct(auction);
+  // finds the seller of the product
+  await sellerOfThisProduct(auction);
+  // adds up the seller email id in the subscribers receiver's string
+  subscribers += sellerEmail;
+  if (auction.startDateTime < new Date()) {
     // notify seller and subscribers that the auction started late
     startAuction(auction);
-  }
-  else {
+
+    // sends emails to the subscribers and seller of the product
+    emailNotification(
+      subscribers,
+      "Sorry! the auction started late",
+      `The auction of ${auction.productName} started late due to a technical glitch. Kindly tune in now as it got started now.`
+    );
+  } else {
     // scheduling the auction to start at its start time
-    const startAuctionJob = schedule.scheduleJob(auction.startDateTime, async function() {
-      startAuction(auction);
-    });
+    const startAuctionJob = schedule.scheduleJob(
+      auction.startDateTime,
+      async function () {
+        startAuction(auction);
+        // sends emails to the subscribers and seller of the product
+        emailNotification(
+          subscribers,
+          "The auction has just started now",
+          `The auction of ${auction.productName} has just started, kindly tune in.`
+        );
+      }
+    );
   }
 }
 
-function scheduleEnd(auction){
-  if(auction.endDateTime<new Date()){
+async function scheduleEnd(auction) {
+  if (auction.endDateTime < new Date()) {
     // ending the auction late
     endAuction(auction);
-  }
-  else {
+    // finds the buyer of the product after the auction ends
+    await findSoldToBuyer(auction);
+    // sends emails to the buyer and seller of the product
+    emailNotification(
+      soldToEmail + "," + sellerEmail,
+      "Sorry! The auction has ended late",
+      `The auction of ${auction.productName} has ended now due to a technical glitch. The product has been sold to ${soldToName} by the seller ${sellerName} at a price of ${productCurrentPrice}`
+    );
+  } else {
     // scheduling the auction to end at its end time
-    const endAuctionJob = schedule.scheduleJob(auction.endDateTime, async function() {
-      endAuction(auction);
-    });
+    const endAuctionJob = schedule.scheduleJob(
+      auction.endDateTime,
+      async function () {
+        endAuction(auction);
+        // finds the buyer of the product after the auction ends
+        await findSoldToBuyer(auction);
+        // sends emails to the buyer and seller of the product
+        emailNotification(
+          soldToEmail + ", " + sellerEmail,
+          "Congratulations! The auction has ended",
+          `The auction of ${auction.productName} has ended now. The product has been sold to ${soldToName} by the seller ${sellerName} at a price of ${productCurrentPrice}`
+        );
+      }
+    );
   }
 }
 
- function scheduleAll () {
+function scheduleAll() {
+  async function auctionsStartScheduler() {
+    // finding all auctions that will start within 1 hour
+    const auctions = await Auction.find({
+      startDateTime: {
+        //  $gte: new Date(),
+        $lte: new Date(new Date().getTime() + 60 * 60 * 1000),
+      },
+      auctionStarted: false,
+    });
+    console.log("start-", auctions.length);
 
-    async function auctionsStartScheduler(){
-        // finding all auctions that will start within 1 hour
-        const auctions= await Auction.find({
-            startDateTime: {
-              //  $gte: new Date(),
-               $lte: new Date(new Date().getTime() + 60 * 60 * 1000)
-            },
-            auctionStarted: false
-         });
-         console.log("start-", auctions.length);
-
-         for (var i = 0; i < auctions.length; i++) {
-          scheduleStart(auctions[i]);
-          }
+    for (var i = 0; i < auctions.length; i++) {
+      scheduleStart(auctions[i]);
     }
+  }
 
-    async function auctionsEndScheduler(){
-        // find all auctions that will end within 1 hour
-        const auctions= await Auction.find({
-            endDateTime: {
-              //  $gte: new Date(),
-               $lte: new Date(new Date().getTime() + 60 * 60 * 1000)
-            },
-            auctionEnded: false
-         });
-         console.log("end-", auctions.length);
+  async function auctionsEndScheduler() {
+    // find all auctions that will end within 1 hour
+    const auctions = await Auction.find({
+      endDateTime: {
+        //  $gte: new Date(),
+        $lte: new Date(new Date().getTime() + 60 * 60 * 1000),
+      },
+      auctionEnded: false,
+    });
+    console.log("end-", auctions.length);
 
-         for (var i = 0; i < auctions.length; i++) {
-            scheduleEnd(auctions[i]);
-          }
- 
+    for (var i = 0; i < auctions.length; i++) {
+      scheduleEnd(auctions[i]);
     }
+  }
 
+  auctionsStartScheduler();
+  auctionsEndScheduler();
+
+  // running auctions start and end schedulers every 50 mins
+  let startTimeInterval = setInterval(async () => {
     auctionsStartScheduler();
     auctionsEndScheduler();
-
-    // running auctions start and end schedulers every 50 mins
-    let startTimeInterval = setInterval(async() => {
-
-        auctionsStartScheduler();
-        auctionsEndScheduler();
-
-      }, 50* 60 * 1000); //runs every 50 mins
-
-
+  }, 50 * 60 * 1000); //runs every 50 mins
 }
 
-module.exports.scheduleAll= scheduleAll;
-module.exports.scheduleStart= scheduleStart;
-module.exports.scheduleEnd= scheduleEnd;
+module.exports.scheduleAll = scheduleAll;
+module.exports.scheduleReminder = scheduleReminder;
+module.exports.scheduleStart = scheduleStart;
+module.exports.scheduleEnd = scheduleEnd;
--- server/routes/products/postProduct.js
+++ server/routes/products/postProduct.js
@@ -9,13 +9,8 @@ const { User } = require("../../models/user");
 var ObjectId = require("mongoose").Types.ObjectId;
 const { cloudinary } = require("./cloudinary");
 require("dotenv").config();
-<<<<<<< HEAD
-const schedule = require("node-schedule");
-var nodemailer = require("nodemailer");
-=======
 // const schedule = require('node-schedule');
 const auctionsScheduler = require("../auctionSpace/auctionsScheduler");
->>>>>>> f468057b38cf672395bba67d25c782f887961b66
 
 const storage = multer.diskStorage({
   // destination: function(req, file, cb) {
@@ -43,13 +38,7 @@ router.post("/", upload.single("productImage"), async (req, res) => {
   const startDateTime = req.body.startDateTime;
   const seperatedStartDate = startDateTime.split("T");
   const startDate = seperatedStartDate[0];
-  // const auctionStartDate = req.body.auctionStartDate;
-  // const auctionStartTime = req.body.auctionStartTime;
-  // const days = req.body.days;
-  // const hours = req.body.hours;
-  // const minutes = req.body.minutes;
   const endDateTime = req.body.endDateTime;
-  // const duration = { days: days,  hours:hours, minutes: minutes }
 
   const result = await cloudinary.uploader.upload(req.file.path);
 
@@ -70,119 +59,18 @@ router.post("/", upload.single("productImage"), async (req, res) => {
     endDateTime: endDateTime,
     productCurrentPrice: product.productBasePrice,
   }).save();
+
+  //scheduling start and end of this auction
+  auctionsScheduler.scheduleReminder(auction);
+  auctionsScheduler.scheduleStart(auction);
+  auctionsScheduler.scheduleEnd(auction);
+
   //adding ProductID against the specific seller in the user model.
   await User.findOneAndUpdate(
     { _id: ObjectId(sellerId) },
     { $push: { postedProducts: ObjectId(product._id) } }
   );
 
-  const reminderDateTime = new Date(
-    new Date(startDateTime).getTime() - 60 * 60 * 24 * 1000
-  );
-
-  
-  const reminderAuction = schedule.scheduleJob(
-    reminderDateTime,
-    async function () {
-      // const soldTo = await User.findOne({_id: maxBidder});
-      //         const soldToName = soldTo.name;
-      //         // const soldToEmail = soldTo.email;
-      const prodAuction = await Auction.findById({_id: auction._id});
-      const subscribers = prodAuction.subscribers;
-      var receiversList = "";
-      for(let i = 0; i < subscribers.length; i++) {
-        const subscriber = await User.findById({_id: subscribers[i]});
-        receiversList += subscriber.email + ",";        
-      }
-      console.log(receiversList);
-      const soldToEmail = "shreyasristi2003@gmail.com, ss2563@cse.jgec.ac.in";
-      const soldToProduct = product.productName;
-      console.log(soldToEmail, soldToProduct);
-
-      var transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: "auctionwebsitedesisproject@gmail.com",
-          pass: "mcwxzkykchkgwyaj",
-        },
-      });
-
-      var mailOptions = {
-        from: "auctionwebsitedesisproject@gmail.com",
-        to: `${soldToEmail}`,
-        subject: `Reminder! Auction of ${soldToProduct} is about to start in 24 hours`,
-        text: `The auction for ${soldToProduct} is about to start in 24 hours. Kindly tune in when it starts.`,
-      };
-
-      transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Email sent: " + info.response);
-        }
-      });
-    }
-  );
-
-  // scheduling the auction to start at the start time
-  const startAuction = schedule.scheduleJob(startDateTime, async function () {
-    
-    console.log(`Auction has started for ${product.productName}!`);
-  // const subscribers = auction.subscribers;
-  // console.log(subscribers);
-
-    const productImage = result.secure_url;
-
-//Adding product details and sellerID in the product model.
-    const product = await new Product({ ... req.body, productImage: productImage, seller: sellerId}).save();
-
-    const auction = await new Auction({product: product._id, productName:product.productName , startDateTime, startDate:startDate, endDateTime:endDateTime, productCurrentPrice: product.productBasePrice}).save();
-
-    //scheduling start and end of this auction
-    auctionsScheduler.scheduleStart(auction);
-    auctionsScheduler.scheduleEnd(auction);
-    
-//adding ProductID against the specific seller in the user model. 
-    await User.findOneAndUpdate(
-        { _id: ObjectId(sellerId) }, 
-        { $push: { postedProducts: ObjectId(product._id) }}
-    );
-  });
-  // scheduling the auction to end at the end time
-  const endAuction = schedule.scheduleJob(endDateTime, async function () {
-    console.log(`Auction has ended for ${product.productName}!`);
-    const prodAuction = await Auction.findById({_id: auction._id});
-      const subscribers = prodAuction.subscribers;
-      var receiversList = "";
-      for(let i = 0; i < subscribers.length; i++) {
-        const subscriber = await User.findById({_id: subscribers[i]});
-        receiversList += subscriber.email + ", ";        
-      }
-      console.log(receiversList);
-    const currAuction = await Auction.findOneAndUpdate(
-      { _id: auction._id },
-      {
-        auctionEnded: true,
-        auctionLive: false,
-      }
-    );
-    if (currAuction.bids.length) {
-      const maxBidder = currAuction.currentBidder;
-      // product sold to the max bidder
-      await Auction.findOneAndUpdate(
-        { _id: auction._id },
-        {
-          soldTo: maxBidder,
-        }
-      );
-      // adding this product in the purchased products array of the highest bidder
-      await User.findOneAndUpdate(
-        { _id: maxBidder },
-        {
-          $push: { purchasedProducts: ObjectId(product._id) },
-        }
-      );
-
-    res.json(product._id);
+  res.json(product._id);
 });
 module.exports = router;
