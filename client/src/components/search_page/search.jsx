44b23c549fb40ce0e8dbf80066aca55acc21b4c7
RishikaSaria <107002646+RishikaSaria@users.noreply.github.com>
2023-03-18 21:55:59 +0530

    Search page (#43)
    * check search submit
    
    * solved search functionality
    
    * solved search routing
    
    * added search result cards
    
    * added search cards
    
    * added sorting according to base price
    
    * Delete tempCodeRunnerFile.cpp
    
    * Delete tempCodeRunnerFile.exe
    
    * added sorting filters and calender view button
    
    * changed filters and sort
    
    * multiple cities dropdown selection is a problem
    
    * checkboxes used lot of space is being occupied rest is fine
    
    * added savneet wale cards
    
    * modified db query
    
    * distance sorting
    
    * distance implemented display in cards is an issue
    
    * logged in logged out
    
    * authorization distance
    
    * distance wala done
    
    * updated
    
    ---------
    
    Co-authored-by: Rishika Saria <s.rishika@iitg.ac.in>
    Co-authored-by: AkshitaJain1503 <97738278+AkshitaJain1503@users.noreply.github.com>
    Co-authored-by: zugzwang03 <2025.cse63.shreyasarkar@gmail.com>

--- client/src/components/search_page/search.jsx
+++ client/src/components/search_page/search.jsx
@@ -6,64 +6,97 @@ import NavBar from "../navbar/index";
 import { Link } from 'react-router-dom';
 //import React, { Component ,useEffect} from 'react';
 import './App.css';
-
+import Card from "./card";
+import styled from "styled-components";
 
 // this function returns name that has been searched for by the user
 const GetProductname = () => {
     const useQuery = () => new URLSearchParams(useLocation().search);
     const query = useQuery();
     const name = query.get('name');
-    
     return name;
 }
-//fetches all products that match the name from GetProductname()
+//fetches all data that match the name from GetProductname()
 const GetSearchResults = () => {
+    const navigate = useNavigate();
     const name = GetProductname();
-    const [data, setData] = React.useState([]);
-    const [sort, setSort] = React.useState("none");
-    const [selectedCities, setSelectedCities] =React.useState({});
-   // const [Time,setTime] =React.useState("none");
+    const [data, setData] = useState([]);
+    const [sort, setSort] = useState("none");
+    const [showLive, setShowLive] = useState(true);
+    const [showUpcoming, setShowUpcoming] = useState(true);
+    const [showEnded, setShowEnded] = useState(false);
+    const [statusList, setstatusList] = useState(["Present","Future"]);
+    //const history = useHistory();
     useEffect(() => {
-        if(Object.keys(data).length === 0){
-            const url = "http://localhost:3001/api/search?name=" + name;
-            axios
-                .get(url)
-                .then((res) => {
-                    setData(res.data.data);
-                })
+        const fetchdata = async () => {
+            const url = `http://localhost:3001/api/search?name=${name}`;
+            const tokenStr = localStorage.getItem("token");
+            const headers = { "Authorization": "Bearer "+tokenStr };
+            const response = await axios.get(url, { headers });
+            setData(response.data);
+        };
+            fetchdata();
+        }, [ name])
+
+        
+        let LoggedIn= false;
+        //console.log(data);
+        //console.log(typeof(data));
+        if(data.length>0 && data[0].userLoggedIn=== true)
+    {
+        LoggedIn = true;
+    }
+    //console.log(LoggedIn);
+        const handleShowLiveChange = (event) => {
+            const checked = event.target.checked;
+            setShowLive(checked);
+        
+            if (checked) {
+              setstatusList([...statusList, "Present"]);
+            } else {
+              setstatusList(statusList.filter(status => status !== "Present"));
             }
-        }, [data, name])
-        const handleShipment = (e) => {
-            const city = e.target.value;
-            const isChecked = e.target.checked;
-            setSelectedCities((prevSelectedCities) => ({
-              ...prevSelectedCities,
-              [city]: isChecked,
-            }));
           };
-        let ShipmentData = data;
-        if (Object.keys(selectedCities).length > 0) {
-            ShipmentData = data.filter((item) => {
-              const shipmentCity = item.shipment;
-              return (
-                Object.prototype.hasOwnProperty.call(selectedCities, shipmentCity) &&
-                selectedCities[shipmentCity]
-              );
-            });
-          }
+        
+          const handleShowUpcomingChange = (event) => {
+            const checked = event.target.checked;
+            setShowUpcoming(checked);
+            if (checked) {
+                setstatusList([...statusList, "Future"]);
+            } else {
+              setstatusList(statusList.filter(status => status !== "Future"));
+            }
+          };
+
+          const handleShowEndedChange = (event) => {
+            const checked = event.target.checked;
+            setShowEnded(checked);
+            if (checked) {
+                setstatusList([...statusList, "Past"]);
+            } else {
+              setstatusList(statusList.filter(status => status !== "Past"));
+            }
+          };
         const handleSort = (e) => {
+            const selectedSort = e.target.value;
+
+    if (selectedSort === "Distance" && LoggedIn === false) {
+        alert("You need to be logged in to sort by shipment Distance!");
+        navigate(`/login`);
+        return;
+    }
+
             setSort(e.target.value);
         }
-    
-        let sortedData = ShipmentData;
+        let sortedData = data;
         if (sort === "lowToHigh") {
-            sortedData = ShipmentData.sort((a, b) => a.basePrice - b.basePrice);
+            sortedData = data.sort((a, b) => a.basePrice - b.basePrice);
         } else if (sort === "highToLow") {
-            sortedData = ShipmentData.sort((a, b) => b.basePrice - a.basePrice);
+            sortedData = data.sort((a, b) => b.basePrice - a.basePrice);
         }
         else if(sort === "StartTime")
         {
-            sortedData = ShipmentData.sort((a, b) => {
+            sortedData = data.sort((a, b) => {
                 const aStartTime = new Date(a.fStartTime).getTime();
                 const bStartTime = new Date(b.fStartTime).getTime();
                 return aStartTime - bStartTime;
@@ -71,112 +104,117 @@ const GetSearchResults = () => {
     }
     else if(sort === "EndTime")
         {
-            sortedData = ShipmentData.sort((a, b) => {
+            sortedData = data.sort((a, b) => {
                 const aEndTime = new Date(a.fEndTime).getTime();
                 const bEndTime = new Date(b.fEndTime).getTime();
                 return aEndTime - bEndTime;
         });
     }
-        
-        
-        return { data: sortedData, handleSort, handleShipment,name,setSelectedCities};
+    else if(sort === "Distance")
+    {
+        sortedData = data.sort((a, b) => a.dist - b.dist);
+    }
+        return { data: sortedData, handleSort,name,handleShowEndedChange,handleShowLiveChange,handleShowUpcomingChange,statusList};
     };
-    const SearchDetails = () => {
+
+    
+
+
+
+
+     const SearchDetails = () => {
         const navigate = useNavigate();
-        const { data, handleSort ,handleShipment,name,setSelectedCities} = GetSearchResults();
-        if (Object.keys(data).length > 0) {
-
-            const handleOnClickEvent = (product) => {
-                navigate(`/productPage?id=${product.productId}`  );
-              };
-
+    const { data:products, handleSort ,name,handleShowEndedChange,handleShowLiveChange,handleShowUpcomingChange,statusList} = GetSearchResults();
+    
+   // console.log('products', products);
+  if (products.length > 0) {
+        // const handleOnClickEvent = (product) => {
+        //     navigate(`/productPage?id=${product.productId}`  );
+        //   };
+       
+    
             return (
-                <div>
-                    <NavBar />
-                    <h4>Search Results for : '{name}'</h4>
-                    <h5>Total Matching Products: {data.length}</h5>
-
-                    <div className='cont'>
-  <label htmlFor="shipment-cities">Select shipment cities:</label>
-  <select onChange={handleSort}>
-                            <option value="none" >Sort by:</option>
-                            <option value="lowToHigh">Price Low to High</option>
-                            <option value="highToLow">Price High to Low</option>
-                            <option value="StartTime">Start Time</option>
-                            <option value="EndTime">End Time</option>
-                        </select>
-  <div  onChange={handleShipment}>
-    <input type="checkbox" id="Hyderabad" name="shipment-cities[]" value="Hyderabad" />
-    <label htmlFor="Hyderabad">Hyderabad</label>
-  </div>
-  <div onChange={handleShipment}>
-    <input type="checkbox" id="Pune" name="shipment-cities[]" value="Pune" />
-    <label htmlFor="Pune">Pune</label>
-  </div >
-  <div onChange={handleShipment}>
-    <input type="checkbox" id="Surat" name="shipment-cities[]" value="Surat" />
-    <label htmlFor="Surat">Surat</label>
-  </div>
-  <div onChange={handleShipment}>
-    <input type="checkbox" id="houston" name="shipment-cities[]" value="Houston" />
-    <label htmlFor="houston">Houston</label>
-  </div>
-  <div onChange={handleShipment}>
-    <input type="checkbox" id="miami" name="shipment-cities[]" value="Miami" />
-    <label htmlFor="miami">Miami</label>
-  </div>
-
-
-
-                        <a href={`/calendarView?name=${name}`}>
-					<button className="white_btn">
-						Calendar
-					</button>
-				</a>
-                    </div>
-                    <hr></hr>
-                    <ul className="card-grid">
-                        {data.map((data) => (
-                            <li>
-                                <div onClick={() => handleOnClickEvent(data)} >
-                                    <article className="card" key={data.productId}>
-                                    <div className="card-image">
-                                        <img src={data.img} alt={data.productName} />
-                                    </div>
-                                    <div className="card-content">
-                                        <h2 className="card-name">{data.productName}</h2>
-                                        <ol className="card-list">
-                                            <ul>
-                                                Base Price :
-                                                { <span>{ data.basePrice}</span> }
-                                            </ul>
-                                            <ul>
-                                                Shipment From: {data.shipment}
-                                            </ul>
-                                            <ul>
-                                                Start Time: {data.StartTime}
-                                            </ul>
-                                            <ul>
-                                                End Time :  {data.EndTime}
-                                            </ul>
-                                        </ol>
-                                        </div>
-                                </article>
+                                <div >
+                                    <NavBar />
+                                    <h4>Search Results for : '{name}'</h4>
+                                    {/* <h5>Total Matching data: {products.length}</h5> */}
+                
+                                    <div className='cont'>
+                    <select onChange={handleSort}>
+                                            <option value="none" >Sort by:</option>
+                                            <option value="lowToHigh">Price Low to High</option>
+                                            <option value="highToLow">Price High to Low</option>
+                                            <option value="StartTime">Start Time</option>
+                                            <option value="EndTime">End Time</option>
+                                            <option value="Distance">Distance</option>
+                                        </select>
+                                        <a href={`/calendarView?name=${name}`}>
+                					<button className="white_btn">
+                						Calendar
+                					</button>
+                				</a>
                                 </div>
-                        </li>
-                    ))}
-                </ul>
+                                <div>
+                                <label>
+                                    <input
+                                    type="checkbox"
+                                    //checked={showEnded}
+                                    onChange={handleShowEndedChange}
+                                    />
+                                    Show ended
+                                </label>
+                                <label>
+                                    <input
+                                    type="checkbox"
+                                    //checked='true'
+                                    defaultChecked={true} 
+                                    onChange={handleShowLiveChange}
+                                    />
+                                    Show live
+                                </label>
+                                <label>
+                                    <input
+                                    type="checkbox"
+                                    //checked={showUpcoming}
+                                    //checked='true'
+                                    defaultChecked={true} 
+                                    onChange={handleShowUpcomingChange}
+                                    />
+                                    Show upcoming
+                                </label>
+                               
+                                </div>
+            <Content >
+                {products.filter((product) => statusList.includes(product.status)).map((product) => (
+                    <Card
+                        key={product.SNo}
+                        Product={product}
+                    />
+                ))}
+            </Content>
             </div>
-        );
-    } 
-     else {
-        return (
-            <div>
-                <NavBar />
-                <h5>Sorry, No products match your search results</h5>
-            </div>
-        );
+         );
+    }
+    else{
+        return(
+            <h5>Sorry,No matching products.</h5>
+        )
     }
 }
 
+
+const Content = styled.div`
+  display: grid;
+  justify-content: center;
+  place-items: center;
+  width: 100%;
+  grid-auto-rows: 420px;
+  grid-template-columns: repeat(4, 280px);
+ grid-gap: 20px;
+ 
+  
+ background-color: rgb(220, 218, 218);
+ 
+  }
+`;
 export default SearchDetails;
--- server/routes/homePage/homeLiveAuctions.js
+++ server/routes/homePage/homeLiveAuctions.js
@@ -39,3 +39,4 @@ router.get("/", async (req, res) => {
     }
   });
   module.exports=router;
+  //
