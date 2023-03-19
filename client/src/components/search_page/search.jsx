66bbe70e22f678aa16df869069aa558b3814b658
RishikaSaria <107002646+RishikaSaria@users.noreply.github.com>
2023-03-19 10:15:38 +0530

    Search page (#44)
    * check search submit
    
    * solved search functionality
    
    * solved search routing
    
    * added search result cards
    
    * added search cards
    
    * added sorting according to base price
    
    * Delete tempCodeRunnerFile.cpp
    
    * Delete tempCodeRunnerFile.exe
    
    * added sorting filters and calender view button
    
    * changed filters and sort
    
    * multiple cities dropdown selection is a problem
    
    * checkboxes used lot of space is being occupied rest is fine
    
    * added savneet wale cards
    
    * modified db query
    
    * distance sorting
    
    * distance implemented display in cards is an issue
    
    * logged in logged out
    
    * authorization distance
    
    * distance wala done
    
    * updated
    
    * css
    
    * navbar in else
    
    ---------
    
    Co-authored-by: Rishika Saria <s.rishika@iitg.ac.in>
    Co-authored-by: AkshitaJain1503 <97738278+AkshitaJain1503@users.noreply.github.com>
    Co-authored-by: zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
    Co-authored-by: Shreya Sarkar <95460021+zugzwang03@users.noreply.github.com>

--- client/src/components/search_page/search.jsx
+++ client/src/components/search_page/search.jsx
@@ -136,7 +136,7 @@ const GetSearchResults = () => {
             return (
                                 <div >
                                     <NavBar />
-                                    <h4>Search Results for : '{name}'</h4>
+                                    <h1>Search Results for : '{name}'</h1>
                                     {/* <h5>Total Matching data: {products.length}</h5> */}
                 
                                     <div className='cont'>
@@ -153,15 +153,13 @@ const GetSearchResults = () => {
                 						Calendar
                 					</button>
                 				</a>
-                                </div>
-                                <div>
                                 <label>
                                     <input
                                     type="checkbox"
                                     //checked={showEnded}
                                     onChange={handleShowEndedChange}
                                     />
-                                    Show ended
+                                    Show ended     
                                 </label>
                                 <label>
                                     <input
@@ -170,7 +168,7 @@ const GetSearchResults = () => {
                                     defaultChecked={true} 
                                     onChange={handleShowLiveChange}
                                     />
-                                    Show live
+                                    Show live    
                                 </label>
                                 <label>
                                     <input
@@ -180,9 +178,8 @@ const GetSearchResults = () => {
                                     defaultChecked={true} 
                                     onChange={handleShowUpcomingChange}
                                     />
-                                    Show upcoming
+                                    Show upcoming  {" "}  
                                 </label>
-                               
                                 </div>
             <Content >
                 {products.filter((product) => statusList.includes(product.status)).map((product) => (
@@ -197,7 +194,11 @@ const GetSearchResults = () => {
     }
     else{
         return(
+            <div>
+            <   NavBar />
             <h5>Sorry,No matching products.</h5>
+            </div>
+          
         )
     }
 }
