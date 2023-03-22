4a3830e9af292d4b046a3702a16ee84b9e861cfc
Savneet <2021ucs0115@iitjammu.ac.in>
2023-03-22 16:48:31 +0530

    unread chats higlighted

---
+++ client/src/components/buyerQueries/index.jsx
@@ -0,0 +1,141 @@
+import React from 'react'
+import { useState } from "react";
+import styled from "styled-components";
+import { GetProfile } from "../myProfile/getProfile";
+import NavBar from '../navbar';
+import Contact from './contacts';
+export default function MyChat() {
+    const [buyerContacts,setBuyerContacts]=useState({});
+    const [sellerContacts,setSellerContacts]=useState({});
+    let user=GetProfile();
+    let id=(user._id);
+    const getBuyerContacts = async () => {
+      if(Object.keys(buyerContacts).length === 0){
+      console.log(id);
+      fetch("http://localhost:3001/api/myChats?id=" + id , {
+            headers: { "Authorization": "Bearer "+localStorage.getItem("token")}
+          })
+        .then(response => response.json())
+        .then(data => 
+          {
+            setBuyerContacts(data);
+          }
+        )
+        .catch(error => console.error(error));
+      }
+  }
+  const getSellerContacts = async () => {
+    if(Object.keys(sellerContacts).length === 0){
+    console.log(id);
+    fetch("http://localhost:3001/api/myChats/seller?id=" + id , {
+          headers: { "Authorization": "Bearer "+localStorage.getItem("token")}
+        })
+      .then(response => response.json())
+      .then(data => 
+        {
+          setSellerContacts(data);
+        }
+      )
+      .catch(error => console.error(error));
+      console.log("hi");
+    }
+}
+    if(id){
+      getBuyerContacts();
+      getSellerContacts();
+    }
+    
+      // if(Object.keys(buyerContacts).length <= 0){
+      //   return(
+      //       <div>
+      //         <NavBar/>
+      //         <h1>Your Contacts</h1>
+      //           <h3>Sorry,you haven't been contacted by anyone yet.</h3>
+      //       </div>
+      //   )
+      // }
+        return (
+            <Container>
+                <NavBar/>
+              <h1>Buyer Contacts</h1>
+              <Contact
+                contacts={buyerContacts} str="buyer"/>
+              <h1>Seller Contacts</h1>
+              <Contact
+                contacts={sellerContacts} str="seller"/>
+              {/* <NavBar/>
+              <h1>Buyer Contacts</h1>
+              {!buyerContacts &&
+              <h3>Sorry,you haven't been contacted by anyone yet.</h3>}
+                    {buyerContacts &&
+                buyerContacts?.map((contact) => (
+                    <Hbar 
+                      key={contact._id}
+                      >
+                        <h4>{contact.contactName}</h4>
+                        {!contact.userReadStatus && <h5>New Messages!</h5>}
+                        {contact.userReadStatus && <h5>no new messages</h5>}
+                        <a href={"/chat?id="+contact.contactUserId+"&name="+contact.contactName+"&bool=f"}>
+                        <Button>Chat with buyer</Button>
+                        </a>
+                        </Hbar>
+                ))}
+                <h1>Seller Contacts</h1>
+                {!sellerContacts &&
+              <h3>Sorry,you haven't been contacted by anyone yet.</h3>}
+                {sellerContacts &&
+                sellerContacts?.map((contact) => (
+                    <Hbar 
+                      key={contact._id}
+                      >
+                        <h4>{contact.contactName}</h4>
+                        {!contact.userReadStatus && <h5>New Messages!</h5>}
+                        {contact.userReadStatus && <h5>no new messages</h5>}
+                        <a href={"/chat?id="+contact.contactUserId+"&name="+contact.contactName+"&bool=f"}>
+                        <Button>Chat with seller</Button>
+                        </a>
+                        </Hbar>
+                ))} */}
+            </Container>
+        )
+
+}
+
+const Hbar = styled.div`
+  margin-top:5px;
+  margin-bottom: 15px;
+  outline-style:solid;
+	width: 100%;
+  max-width:5000px;
+	height: 60px;
+	background-color: #ffffff;
+	display: flex;
+	align-items: center;
+  h4{
+    margin-left:auto;
+    margin-right:auto;
+    color:#334b48
+  }
+  h5{
+    color: #78E9A6;
+    margin-right: 50px;
+  }
+  justify-content: space-between;
+`;
+const Button = styled.button`
+  border: none;
+	outline: none;
+	padding: 1px 0;
+	background-color: #3bb19b; 
+	border-radius: 20px;
+	width: 100px;
+	font-weight: bold;
+	font-size: 14px;
+	cursor: pointer;
+	// margin-right: auto;
+	margin-left:auto;
+`
+const Container =styled.div`
+  background-color: rgb(234, 237, 237);
+  height: 100dvh;
+  `
